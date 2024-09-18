668558b7910932e80d8ae2aeba39485f849ec362
claytons6749 <claytons6749@student.faytechcc.edu>
2024-09-18 19:28:19 -0400

    Add files via upload

---
+++ P1LAB1_ClaytonStar.py
@@ -0,0 +1,10 @@
+# Star Clayton
+# 13 September 2024
+# P1LAB1
+# Get input from user and output the information in a certain format.
+
+# The input function always save the data as a string by default
+first_name = input("Enter  your first name:")
+last_name = input("Enter your last name:")
+print("Hello", first_name, last_name + "!", "Welcome to CTI-110")
+print(type(first_name))
