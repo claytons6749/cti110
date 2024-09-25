e7d21dac1a54b14be0f5197cd2fe5ddc967e14e3
claytons6749 <claytons6749@student.faytechcc.edu>
2024-09-25 17:18:34 -0400

    Add files via upload

---
+++ P1WH2_ClaytonStar.py
@@ -0,0 +1,17 @@
+ # Star Clayton
+ # 22 sept 2024
+ # 1PHW2
+ # Calcute travel expenses
+
+# Calculating travel budget
+
+("This prgram calculates and displays travel expenses")
+amount = int(input("enter budget:"))
+dest = input("Enter your travel destination:")
+gas =  int(input("How much do you think you will spend on gas?"))
+hotel = int(input("How much do you think you will spend on Hotel?:"))
+food = int(input("Last, how much do you need for food?:"))
+
+#output
+print("----Travel)--Expenses")
+print(f{"Location': <30}", f"{ dest : <20}')
