646f8dca54ac6d4b3b5a20271a8ffec856189369
claytons6749 <claytons6749@student.faytechcc.edu>
2024-09-25 23:04:18 +0000

    Submission 2 update

--- P1HW2_ClaytonStar.py
+++ P1HW2_ClaytonStar.py
@@ -3,13 +3,22 @@
 # P1HW2
 #  Using mathematical equation in code for travel expenses.
 
+# Get input from users
 print ("---This program calcutes and display travel expenses---")
 print()
-bud = int(input("Enter your budget:"))
+bud = float(input("Enter your budget:"))
 dest = input("Enter Destination:")
-gas = int(input("Enter what you will spend on gas:"))
-acc = int(input("Enter what you think you will spend on accommodation:"))
-food = int(input("Enter what you spend on food:"))
+gas = float(input("Enter what you will spend on gas:"))
+acc = float(input("Enter what you think you will spend on accommodation:"))
+food = float(input("Enter what you spend on food:"))
+
+# Calculate planned budget
+bud2 = gas + acc + food
+
+# Calculate remaining budget
+bal = bud - gas - acc - food
+
+#output values
 print()
 print("------Travel Expenses-------")
 print()
