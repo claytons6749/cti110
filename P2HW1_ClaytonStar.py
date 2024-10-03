f54e622e9cddf1f344b48c9309c6491ac82aa76d
claytons6749 <claytons6749@student.faytechcc.edu>
2024-10-03 09:37:43 -0400

    Create P2HW1_ClaytonStar.py

---
+++ P2HW1_ClaytonStar.py
@@ -0,0 +1,31 @@
+# Star Clayton
+# September 19, 2024
+# P1HW1
+#  Using mathematical equation in code for travel expenses.
+
+# Get Inputs from users
+print ("---This program calcutes and display travel expenses---")
+print()
+bud = float(input("Enter your budget:"))
+dest = input("Enter Destination:")
+gas = float(input("Enter what you will spend on gas:"))
+acc = float(input("Enter what you think you will spend on accommodation:"))
+food = float(input("Enter what you spend on food:"))
+
+# Calculate the total planned budget
+bud2 = gas + acc + food
+
+# Calculate the remaining balance
+bal = bud - gas - acc - food
+
+# Output the balances
+print()
+print("------Travel Expenses-------")
+print()
+print(f"Location: {dest}")
+print(f"Inital Buget: ${bud:.2f}")
+print()
+print(f"Fuel: ${gas:.2f}")
+print(f"Accommodation: ${acc:.2f}")
+print(f"Food: ${food:.2f}")
+print()
