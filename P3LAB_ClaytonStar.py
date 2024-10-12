777dae7d944aa14b76f358c8d45880e813e379fa
claytons6749 <claytons6749@student.faytechcc.edu>
2024-10-12 16:35:25 -0400

    Create P3LAB_ClaytonStar.py

---
+++ P3LAB_ClaytonStar.py
@@ -0,0 +1,17 @@
+#Star Clayton
+#October 19, 2024
+#P3LAB
+# Calculates the amount of money into Dollars, Quarters, Dimes, Nickels, and Pennies.
+
+#Enter calculations amount$52,22 in dollars, Quarters, Dimes, Nickels, and pennies.
+price = $52.22
+decimal_places = .2f
+
+money = ['dollars', 'quarters', 'Dimes', 'Nickels','Pennies']
+
+print(money)
+#Calculate the cost costs into the right money catoriges.
+
+print("Enter the amount to be calculated")
+print("You have $52.22 sort into the corrected money cateriges.")
+
