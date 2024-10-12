54d981bec72b5f4383b4ae023ad0eadac14c2011
claytons6749 <claytons6749@student.faytechcc.edu>
2024-10-12 15:45:16 -0400

    Add files via upload

---
+++ P2Lab2_ClaytonStar.py Idle shell.py
@@ -0,0 +1,196 @@
+Python 3.12.6 (v3.12.6:a4a2d2b0d85, Sep  6 2024, 16:08:03) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+==== RESTART: /Users/starclayton/Desktop/FTCC/CTI 110/P2Lab2_ClaytonStar.py ====
+dict_keys(['Camaro', 'Prius', 'Model S', 'Silverado'])
+
+==== RESTART: /Users/starclayton/Desktop/FTCC/CTI 110/P2Lab2_ClaytonStar.py ====
+dict_keys(['Camaro', 'Prius', 'Model S', 'Silverado'])
+Traceback (most recent call last):
+  File "/Users/starclayton/Desktop/FTCC/CTI 110/P2Lab2_ClaytonStar.py", line 13, in <module>
+    print(*list_name, sep = ",")
+NameError: name 'list_name' is not defined
+
+==== RESTART: /Users/starclayton/Desktop/FTCC/CTI 110/P2Lab2_ClaytonStar.py ====
+dict_keys(['Camaro', 'Prius', 'Model S', 'Silverado'])
+Traceback (most recent call last):
+  File "/Users/starclayton/Desktop/FTCC/CTI 110/P2Lab2_ClaytonStar.py", line 13, in <module>
+    print(*list_name, sep = ", ")
+NameError: name 'list_name' is not defined
+
+==== RESTART: /Users/starclayton/Desktop/FTCC/CTI 110/P2Lab2_ClaytonStar.py ====
+dict_keys(['Camaro', 'Prius', 'Model S', 'Silverado'])
+Traceback (most recent call last):
+  File "/Users/starclayton/Desktop/FTCC/CTI 110/P2Lab2_ClaytonStar.py", line 13, in <module>
+    print(*list_name, sep = " , ")
+NameError: name 'list_name' is not defined
+
+==== RESTART: /Users/starclayton/Desktop/FTCC/CTI 110/P2Lab2_ClaytonStar.py ====
+dict_keys(['Camaro', 'Prius', 'Model S', 'Silverado'])
+Traceback (most recent call last):
+  File "/Users/starclayton/Desktop/FTCC/CTI 110/P2Lab2_ClaytonStar.py", line 13, in <module>
+    print(*list_name, sep = " , ")
+NameError: name 'list_name' is not defined
+
+==== RESTART: /Users/starclayton/Desktop/FTCC/CTI 110/P2Lab2_ClaytonStar.py ====
+dict_keys(['Camaro', 'Prius', 'Model S', 'Silverado'])
+Traceback (most recent call last):
+  File "/Users/starclayton/Desktop/FTCC/CTI 110/P2Lab2_ClaytonStar.py", line 13, in <module>
+    print(*list_name,sep = " , ")
+NameError: name 'list_name' is not defined
+
+==== RESTART: /Users/starclayton/Desktop/FTCC/CTI 110/P2Lab2_ClaytonStar.py ====
+dict_keys(['Camaro', 'Prius', 'Model S', 'Silverado'])
+Traceback (most recent call last):
+  File "/Users/starclayton/Desktop/FTCC/CTI 110/P2Lab2_ClaytonStar.py", line 13, in <module>
+    print(*list_name,sep = ' , ')
+NameError: name 'list_name' is not defined
+
+==== RESTART: /Users/starclayton/Desktop/FTCC/CTI 110/P2Lab2_ClaytonStar.py ====
+dict_keys(['Camaro', 'Prius', 'Model S', 'Silverado'])
+Traceback (most recent call last):
+  File "/Users/starclayton/Desktop/FTCC/CTI 110/P2Lab2_ClaytonStar.py", line 13, in <module>
+    print(list_name,sep = ' , ')
+NameError: name 'list_name' is not defined
+
+==== RESTART: /Users/starclayton/Desktop/FTCC/CTI 110/P2Lab2_ClaytonStar.py ====
+dict_keys(['Camaro', 'Prius', 'Model S', 'Silverado'])
+Traceback (most recent call last):
+  File "/Users/starclayton/Desktop/FTCC/CTI 110/P2Lab2_ClaytonStar.py", line 13, in <module>
+    print(*list_name, sep = ' , ')
+NameError: name 'list_name' is not defined
+
+==== RESTART: /Users/starclayton/Desktop/FTCC/CTI 110/P2Lab2_ClaytonStar.py ====
+dict_keys(['Camaro', 'Prius', 'Model S', 'Silverado'])
+Camaro , Prius , Model S , Silverado
+
+==== RESTART: /Users/starclayton/Desktop/FTCC/CTI 110/P2Lab2_ClaytonStar.py ====
+dict_keys(['Camaro', 'Prius', 'Model S', 'Silverado'])
+Camaro , Prius , Model S , Silverado
+Enter a car:Camaro
+Traceback (most recent call last):
+  File "/Users/starclayton/Desktop/FTCC/CTI 110/P2Lab2_ClaytonStar.py", line 19, in <module>
+    car_mpg = car[car_name]
+NameError: name 'car' is not defined. Did you mean: 'cars'?
+
+==== RESTART: /Users/starclayton/Desktop/FTCC/CTI 110/P2Lab2_ClaytonStar.py ====
+dict_keys(['Camaro', 'Prius', 'Model S', 'Silverado'])
+Camaro , Prius , Model S , Silverado
+Enter a car:Prius
+Traceback (most recent call last):
+  File "/Users/starclayton/Desktop/FTCC/CTI 110/P2Lab2_ClaytonStar.py", line 19, in <module>
+    car_mpg = car[car_name]
+NameError: name 'car' is not defined. Did you mean: 'cars'?
+
+==== RESTART: /Users/starclayton/Desktop/FTCC/CTI 110/P2Lab2_ClaytonStar.py ====
+dict_keys(['Camaro', 'Prius', 'Model S', 'Silverado'])
+Camaro , Prius , Model S , Silverado
+Enter a car:Prius
+Traceback (most recent call last):
+  File "/Users/starclayton/Desktop/FTCC/CTI 110/P2Lab2_ClaytonStar.py", line 19, in <module>
+    car_mpg = car[car_name]
+NameError: name 'car' is not defined. Did you mean: 'cars'?
+
+==== RESTART: /Users/starclayton/Desktop/FTCC/CTI 110/P2Lab2_ClaytonStar.py ====
+dict_keys(['Camaro', 'Prius', 'Model S', 'Silverado'])
+Camaro , Prius , Model S , Silverado
+Enter a car:Prius
+The Prius get 52.36 miles per gallon.
+
+==== RESTART: /Users/starclayton/Desktop/FTCC/CTI 110/P2Lab2_ClaytonStar.py ====
+dict_keys(['Camaro', 'Prius', 'Model S', 'Silverado'])
+Camaro , Prius , Model S , Silverado
+Enter a car:Prius
+The Prius get 52.36 miles per gallon.
+How many miles will you drive the {car_name}?
+==== RESTART: /Users/starclayton/Desktop/FTCC/CTI 110/P2Lab2_ClaytonStar.py ====
+dict_keys(['Camaro', 'Prius', 'Model S', 'Silverado'])
+Camaro , Prius , Model S , Silverado
+Enter a car:Silverado
+The Silverado get 26 miles per gallon.
+How many miles will you drive the {car_name}?375.5
+Traceback (most recent call last):
+  File "/Users/starclayton/Desktop/FTCC/CTI 110/P2Lab2_ClaytonStar.py", line 29, in <module>
+    print(f"{gallons_needed} gallon(s) of gas are needed to drive the  {car_name} {miles_driven} miles")
+NameError: name 'gallons_needed' is not defined. Did you mean: 'gallon_needed'?
+
+==== RESTART: /Users/starclayton/Desktop/FTCC/CTI 110/P2Lab2_ClaytonStar.py ====
+dict_keys(['Camaro', 'Prius', 'Model S', 'Silverado'])
+Camaro , Prius , Model S , Silverado
+Enter a car: Silverado
+Traceback (most recent call last):
+  File "/Users/starclayton/Desktop/FTCC/CTI 110/P2Lab2_ClaytonStar.py", line 19, in <module>
+    car_mpg = cars[car_name]
+KeyError: ' Silverado'
+
+==== RESTART: /Users/starclayton/Desktop/FTCC/CTI 110/P2Lab2_ClaytonStar.py ====
+dict_keys(['Camaro', 'Prius', 'Model S', 'Silverado'])
+Camaro , Prius , Model S , Silverado
+Enter a car:Silverado
+The Silverado get 26 miles per gallon.
+How many miles will you drive the {car_name}?375.5
+Traceback (most recent call last):
+  File "/Users/starclayton/Desktop/FTCC/CTI 110/P2Lab2_ClaytonStar.py", line 29, in <module>
+    print(f"{gallons_needed} gallon(s) of gas are needed to drive the {car_name} {miles_driven} miles")
+NameError: name 'gallons_needed' is not defined. Did you mean: 'gallon_needed'?
+
+==== RESTART: /Users/starclayton/Desktop/FTCC/CTI 110/P2Lab2_ClaytonStar.py ====
+dict_keys(['Camaro', 'Prius', 'Model S', 'Silverado'])
+Camaro , Prius , Model S , Silverado
+Enter a car:Silverado
+The Silverado get 26 miles per gallon.
+How many miles will you drive the {car_name}?
+==== RESTART: /Users/starclayton/Desktop/FTCC/CTI 110/P2Lab2_ClaytonStar.py ====
+dict_keys(['Camaro', 'Prius', 'Model S', 'Silverado'])
+Camaro , Prius , Model S , Silverado
+Enter a car:Silverado
+The Silverado get 26 miles per gallon.
+How many miles will you drive the {car_name}?
+==== RESTART: /Users/starclayton/Desktop/FTCC/CTI 110/P2Lab2_ClaytonStar.py ====
+dict_keys(['Camaro', 'Prius', 'Model S', 'Silverado'])
+Camaro , Prius , Model S , Silverado
+Enter a car:Silverado
+The Silverado gets 26 miles per gallon.
+How many miles will you drive the Silverado?375.5
+14.442307692307692 gallon(s) of gas are needed to drive the (car_name) 375.5 miles
+
+==== RESTART: /Users/starclayton/Desktop/FTCC/CTI 110/P2Lab2_ClaytonStar.py ====
+dict_keys(['Camaro', 'Prius', 'Model S', 'Silverado'])
+Camaro , Prius , Model S , Silverado
+Enter a car:Silverado
+The Silverado gets 26 miles per gallon.
+How many miles will you drive the Silverado?375.5
+14.44 gallon(s) of gas are needed to drive the (car_name) 375.5 miles
+>>> 
+==== RESTART: /Users/starclayton/Desktop/FTCC/CTI 110/P2Lab2_ClaytonStar.py ====
+dict_keys(['Camaro', 'Prius', 'Model S', 'Silverado'])
+Camaro , Prius , Model S , Silverado
+Enter a car:Camaro
+The Camaro gets 18.21 miles per gallon.
+How many miles will you drive the Camaro?18.21
+1.00 gallon(s) of gas are needed to drive the (car_name) 18.21 miles
+>>> 
+==== RESTART: /Users/starclayton/Desktop/FTCC/CTI 110/P2Lab2_ClaytonStar.py ====
+dict_keys(['Camaro', 'Prius', 'Model S', 'Silverado'])
+Camaro , Prius , Model S , Silverado
+Enter a car:Prius
+The Prius gets 52.36 miles per gallon.
+How many miles will you drive the Prius?52.36
+1.00 gallon(s) of gas are needed to drive the (car_name) 52.36 miles
+>>> 
+==== RESTART: /Users/starclayton/Desktop/FTCC/CTI 110/P2Lab2_ClaytonStar.py ====
+dict_keys(['Camaro', 'Prius', 'Model S', 'Silverado'])
+Camaro , Prius , Model S , Silverado
+Enter a car:Pruis
+Traceback (most recent call last):
+  File "/Users/starclayton/Desktop/FTCC/CTI 110/P2Lab2_ClaytonStar.py", line 19, in <module>
+    car_mpg = cars[car_name]
+KeyError: 'Pruis'
+>>> 
+==== RESTART: /Users/starclayton/Desktop/FTCC/CTI 110/P2Lab2_ClaytonStar.py ====
+dict_keys(['Camaro', 'Prius', 'Model S', 'Silverado'])
+Camaro , Prius , Model S , Silverado
+Enter a car:Prius
+The Prius gets 52.36 miles per gallon.
+How many miles will you drive the Prius?50
+0.95 gallon(s) of gas are needed to drive the (car_name) 50.0 miles
